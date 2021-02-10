b259a3d97deeffa5f2bb4f566a4c85df1c8ddbd2
Tarciso Torres <tarcisoftorres@gmail.com>
2021-02-10 14:47:38 -0300

    test: deve validar campos obrigatórios da movimentação

--- src/main/java/br/ce/redfort/rest/tests/BarrigaTest.java
+++ src/main/java/br/ce/redfort/rest/tests/BarrigaTest.java
@@ -105,8 +105,30 @@ public class BarrigaTest extends BaseTest{
 		.when()
 			.post("/transacoes")
 		.then()
-			.log().all()
 			.statusCode(201)
+		;
+	}
+	
+	@Test
+	public void deveValidarCamposObrigatoriosDaMovimentacao() {
+		given()
+		.header("Authorization", "JWT " + TOKEN)
+		.body("{}")
+		.when()
+			.post("/transacoes")
+		.then()
+			.statusCode(400)
+			.body("$", hasSize(8))
+			.body("msg", hasItems(
+					"Data da Movimentação é obrigatório",
+					"Data do pagamento é obrigatório",
+					"Descrição é obrigatório",
+					"Interessado é obrigatório",
+					"Valor é obrigatório",
+					"Valor deve ser um número",
+					"Conta é obrigatório",
+					"Situação é obrigatório"
+					))
 		;
 	}
 
