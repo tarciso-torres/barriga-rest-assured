5c0bb044f08b2fa2a2dbf983b72443ecc7986404
Tarciso Torres <tarcisoftorres@gmail.com>
2021-02-10 11:33:22 -0300

    test: Deve incluir conta com sucesso

--- src/main/java/br/ce/redfort/rest/tests/BarrigaTest.java
+++ src/main/java/br/ce/redfort/rest/tests/BarrigaTest.java
@@ -1,6 +1,10 @@
 package br.ce.redfort.rest.tests;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -10,12 +14,39 @@ public class BarrigaTest extends BaseTest {
 	
 	@Test
 	public void naoDeveAcessarAPISemToken() {
-		
 		given()
 		.when()
 			.get("/contas")
 		.then()
 			.statusCode(401)
+		;
+	}
+	
+	@Test
+	public void deveIncluirContaComSucesso() {
+		Map<String, String> login = new HashMap<String, String>();
+		login.put("email", "tarciso.test@test.com");
+		login.put("senha", "123456");
+		
+		String token = given()
+			.body(login)
+		.when()
+			.post("/signin")
+		.then()
+			.statusCode(200)
+			.extract().path("token")
+		;
+		
+		given()
+			.header("Authorization", "JWT " + token)
+			.body("{\"nome\": \"Nome Qualquer\"}")
+		.when()
+			.post("/contas")
+		.then()
+		.log().all()
+			.statusCode(201)
+			.body("nome", is("Nome Qualquer"))
+			.body("visivel", is(true))
 		;
 	}
 
